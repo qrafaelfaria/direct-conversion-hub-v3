78808c28a56f342eb6da4ee1ff609ad87968edae
qrafaelfaria <rrfaria2020@gmail.com>
2026-03-30 18:47:36 -0300

    asqwe

--- src/components/FeedbackSection.tsx
+++ src/components/FeedbackSection.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import { motion } from "framer-motion";
-import image1 from "/Pija01.jpg";
-import image7 from "/Pija02.jpg";
-import image8 from "/Pija03.jpg";
-import image20 from "/Pija04.jpg";
-import image44 from "/Pija05.jpg";
-import image69 from "/Pija06.jpg";
 
 const FeedbackSection: React.FC = () => {
   const images = [
@@ -13,11 +7,9 @@ const FeedbackSection: React.FC = () => {
     "/Feed07.png",
     "/Feed08.png",
   ];
-const cards = [image1, image7, image8, image20, image44, image69];
-const duplicated = [...cards, ...cards];
 
   return (
-    <section className="sm:py-20 bg-gray-100">
+    <section className="sm:py-20 bg-gray-100 p-8">
       <div className="section-container">
         <h2 className="text-3xl sm:text-4xl font-display font-black text-center text-foreground mb-10">
           O que nossos clientes dizem
@@ -46,25 +38,6 @@ const duplicated = [...cards, ...cards];
             </a>
           </motion.div>
         </div>
-
-<div className="relative mt-12 bg-gray-100 gap-4">
-        <div className="flex animate-marquee bg-white p-12 gap-4 bg-gray-100"  style={{ width: "fit-content" }}>
-          {duplicated.map((card, i) => (
-            <div
-              key={i}
-              className="flex-shrink-0 w-80 h-80 sm:w-80 sm:h-80 rounded-xl overflow-hidden transition-transform duration-300 bg-gray-100"
-            >
-              <img
-                src={card}
-                alt={`Capa de Caderno Personalizada ${i + 1}`}
-                loading="lazy"
-                className="w-full h-full object-contain block"
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-        
       </div>
     </section>
   );
