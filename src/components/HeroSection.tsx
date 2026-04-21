69547141855a1fa5b8573e0b75383e949141ce8e
qrafaelfaria <rrfaria2020@gmail.com>
2026-04-21 19:22:37 -0300

    dddas

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -48,8 +48,8 @@ const HeroSection = () => {
     <>
       <section className="relative overflow-hidden pt-8 pb-12 sm:pt-12 sm:pb-20 bg-[#c43c6f] text-white">
         {/* Top Urgent Bar */}
-        <div className="bg-[#a02a50] text-white py-2 text-center font-bold uppercase tracking-widest text-3xl bg-urgency">
-          🚨 ESPERE! TEMOS UMA OFERTA EXCLUSIVA PARA VOCÊ
+        <div className="bg-[#a02a50] text-white py-2 text-center font-bold uppercase tracking-widest text-2xl bg-urgency pt-8">
+          🚨 ESPERE! TEMOS UMA OFERTA EXCLUSIVA PARA VOCÊ!
         </div>
 
         {/* Countdown Timer */}
@@ -62,6 +62,14 @@ const HeroSection = () => {
           </div>
         </div>
 
+{/* Progress Bar */}
+            <div className="max-w-sm mx-auto bg-[#a02a50] rounded-full h-8 flex items-center justify-between p-1 m-8">
+              <div className="bg-[#ffe500] h-6 rounded-full flex items-center justify-end pr-2" style={{ width: '90%' }}>
+                <span className="text-[#c43c6f] text-sm font-bold">99%</span>
+              </div>
+            </div>
+
+
         <div className="section-container mt-8">
           <motion.div
             initial={{ opacity: 0, scale: 0.95 }}
@@ -70,15 +78,8 @@ const HeroSection = () => {
             className="text-center max-w-5xl mx-auto"
           >
             <h2 className="text-xl sm:text-2xl font-bold text-white text-balance mb-4">
-              Sua compra está quase finalizada… Mas antes, queremos te oferecer algo que pode <span className="text-[#ffe500]">aumentar sua renda ainda mais. </span>
+              Sua compra está quase finalizada… Mas antes, queremos te oferecer algo que pode <span className="text-[#ffe500]">aumentar ainda mais a sua renda com costura.</span>
             </h2>
-
-            {/* Progress Bar */}
-            <div className="max-w-md mx-auto bg-[#a02a50] rounded-full h-8 flex items-center justify-between p-1 mb-8">
-              <div className="bg-[#ffe500] h-6 rounded-full flex items-center justify-end pr-2" style={{ width: '90%' }}>
-                <span className="text-[#c43c6f] text-sm font-bold">99%</span>
-              </div>
-            </div>
 
             {/* Down Arrows */}
             <div className="flex justify-center my-8">
@@ -88,26 +89,21 @@ const HeroSection = () => {
             </div>
 
             <p className="text-white text-xl sm:text-2xl mb-8">
-              Agora, além do curso de Costura Hospitalar, você também pode obter o Super Pack de Moldes por um valor simbólico.
+              Agora, além do Curso de Costura Hospitalar, você pode garantir acesso ao Super Pack de Moldes Profissionais. <span className="text-[#ffe500]">Um acervo completo para você produzir, variar e vender muito mais.</span> 
             </p>
 
             {/* Product Image */}
             <div className="flex justify-center">
-              <img src="/Moldes.png" alt="Super Pack de Moldes" className="max-w-full h-auto mx-auto" />
+              <img src="/Maisvendido.png" alt="Super Pack de Moldes" className="max-w-full h-auto mx-auto" />
             </div>
 
             {/* Pricing */}
-            <p className="text-white text-2xl sm:text-3xl font-bold mb-8">
+            <p className="text-white text-2xl sm:text-3xl font-bold mt-8">
               De <span className="line-through">R$ 49,90</span> por <span className="text-[#ffe500]">R$ 14,90</span>
             </p>
 
-            {/* Wiapy Upsell Widget */}
-            <div className="mt-12">
-              <div id="wiapy_upsell"></div>
-            </div>
-
             {/* New Section: What you will receive */}
-            <div className="bg-[#fbe8ee] p-6 rounded-lg shadow-lg mt-12 max-w-md mx-auto">
+            <div className="bg-[#fbe8ee] p-6 rounded-lg shadow-lg mt-6 max-w-md mx-auto">
               <h3 className="text-[#c43c6f] text-3xl font-black mb-6 text-center">O que você vai receber:</h3>
               <ul className="space-y-4 mb-8 text-lg">
                 <li className="flex items-left text-[#c43c6f] text-lg">
@@ -135,9 +131,10 @@ const HeroSection = () => {
                   Guia de Impressão
                 </li>
               </ul>
-              <button className="w-full bg-[#c43c6f] text-white font-bold py-4 px-6 rounded-lg text-xl transition-colors duration-300">
-                Garantia de 30 dias ou seu dinheiro de volta!
-              </button>
+            </div>
+{/* Wiapy Upsell Widget */}
+            <div className="mt-8">
+              <div id="wiapy_upsell"></div>
             </div>
 
           </motion.div>
@@ -145,7 +142,7 @@ const HeroSection = () => {
       </section>
       <section className="bg-[#c43c6f] text-white py-8 px-4 text-center">
         <p className="text-lg italic mb-4">
-          Essa é a sua única oportunidade de adquirir esses moldes pelo valor promocional! Ao fechar essa página, o valor voltará ao normal.
+          Essa é a sua única chance de garantir esse pack com desconto. Ao fechar essa página, o valor volta ao normal e essa oferta desaparece.
         </p>
         <div className="flex flex-col items-center justify-center mb-8">
           <img src="/logocostura.png" alt="Curso de Costura Hospitalar" className="w-48 h-auto mx-auto mb-4 rounded-full" />
